b4d8dd52bb2d2dc2191500b6de28ea26d8b4ef60
agiprmsty22-oss <agiprmsty22@gmail.com>
2026-01-15 01:57:21 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,83 @@
+const members = {
+    agi: {
+        nama: "Giw",
+        foto: "images/agi.jpeg",
+        instagram: "https://instagram.com/agipramusty_"
+}
+,
+    rapli: {
+        nama: "Ule",
+        foto: "images/rafli.jpeg",
+        instagram: "https://instagram.com/ali.raflii"
+    },
+    dapa: {
+        nama: "Dapet",
+        foto: "images/dapa.jpeg",
+        instagram: "https://instagram.com/ur.typeshit"
+    },
+    sarah: {
+        nama: "Sas",
+        foto: "images/sas.jpeg",
+        instagram: "https://instagram.com/sarahsas_"
+    },
+    hilda: {
+        nama: "Hilda",
+        foto: "images/hilda.jpeg",
+        instagram: "https://instagram.com/khdnzh_"
+    },
+    aisyah: {
+        nama: "Iseh",
+        foto: "images/iseh.jpeg",
+        instagram: "https://instagram.com/aisyahtsr"
+    },
+    lulu: {
+        nama: "Lulu",
+        foto: "images/lulu.jpeg",
+        instagram: "https://instagram.com/lllulu.u"
+    },
+    askia: {
+        nama: "Aski",
+        foto: "images/aski.jpeg",
+        instagram: "https://instagram.com/azkiazhrr"
+    },
+    adit: {
+        nama: "Adit",
+        foto: "images/adit.jpeg",
+        instagram: "https://instagram.com/aditymriz"
+    },
+    Melya: {
+        nama: "Meli",
+        foto: "images/meli.jpeg",
+        instagram: "https://instagram.com/melyaikbarz"
+    },
+    kiki: {
+        nama: "Kiki",
+        foto: "images/kiki.jpeg",
+        instagram: "https://instagram.com/riskifaa_"
+    },
+    farry: {
+        nama: "Farry",
+        foto: "images/farry.jpeg",
+        instagram: "https://instagram.com/farrynugrh_"
+    },
+    kinan: {
+        nama: "Kinan",
+        foto: "images/kinan.jpeg",
+        instagram: "https://instagram.com/_mrizkyananda"
+    },
+    pungki: {
+        nama: "Ipung",
+        foto: "images/ipung.jpeg",
+        instagram: "https://instagram.com/pungkianggoro"
+    },
+    riri: {
+        nama: "Riri",
+        foto: "images/riri.jpeg",
+        instagram: "https://instagram.com/riri_faqat"
+    },
+    fasya: {
+        nama: "Fasya",
+        foto: "images/fasya.jpeg",
+        instagram: "https://instagram.com/fasyaadithia_"
+    }
+};
---
+++ group.js
@@ -0,0 +1,37 @@
+const groups = {
+    1: {
+        foto: [
+            "images/group1.jpeg"
+        ]
+    },
+    2: {
+        foto: [
+            "images/group2.jpeg"
+        ]
+    },
+    3: {
+        foto: [
+            "images/group3.jpeg"
+        ]
+    },
+    4: {
+        foto: [
+            "images/group4.jpeg"
+        ]
+    },
+    5: {
+        foto: [
+            "images/group5.jpeg"
+        ]
+    },
+    6: {
+        foto: [
+            "images/group6.jpeg"
+        ]
+    },
+    7: {
+        foto: [
+            "images/group7.jpeg"
+        ]
+    }
+};
